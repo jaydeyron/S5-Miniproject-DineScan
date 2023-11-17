926de9a0aa45f7090042aff248dc1e677a5dbbc1
Jay <jaydeyron@gmail.com>
2023-11-17 11:02:23 +0530

    flask

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template 
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = 'hailhitler'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MYSQL_HOST'] = 'localhost'  # Replace with your MySQL host
-app.config['MYSQL_USER'] = 'root'  # Replace with your MySQL username
-app.config['MYSQL_PASSWORD'] = 'jayasankar123'  # Replace with your MySQL password
-app.config['MYSQL_DB'] = 'dinescan'  # Replace with your database name
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'jayasankar123'
+app.config['MYSQL_DB'] = 'dinescan'
 
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
+@app.route("/") 
+def index(): 
+   return render_template("home.html")
+
+@app.route("/login") 
+def index(): 
+   return render_template("login.html")
+
+@app.route('/demo')
+def demo():
     mycurs = mysql.connection.cursor()
     mycurs.execute('SELECT * FROM dishes')
     a = mycurs.fetchall()
     mycurs.close()
     return str(a)
-
-@app.route('/menu')
-def indexb():
-    return "hello"
 
 if __name__ == '__main__':
     app.run(debug=True)
